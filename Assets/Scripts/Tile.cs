1c5cf99d4c475751ebb660c0f8864603f3b92be2
ABEL K ANTONY <abelkantony98@gmail.com>
2023-06-21 20:51:43 +0530

    randomly generate two tiles at the start of the game

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class Tile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TileState state { get; private set; }
+    public TileCell cell { get; private set; }
+    public int number { get; private set; }
+
+    private Image background;
+    private TextMeshProUGUI text;
+
+    private void Awake()
     {
-        
+        background = GetComponent<Image>();
+        text = GetComponentInChildren<TextMeshProUGUI>();
+    }
+    public void SetState(TileState state, int number)
+    {
+        this.state = state;
+        this.number = number;
+
+        background.color = state.backgroundColor;
+        text.color = state.textColor;
+        text.text = number.ToString();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Spawn(TileCell cell)
     {
-        
+        if(this.cell !=null)
+        {
+            this.cell = null;
+        }
+        this.cell = cell;
+        this.cell.tile = this;
+
+        transform.position = cell.transform.position;
     }
 }
