e3aedc09e6f3a52ab5963b41b6a84c0dd6779584
ABEL K ANTONY <abelkantony98@gmail.com>
2023-06-25 15:06:22 +0530

    update the merging of cell having same value

--- Assets/Scripts/TileBoard.cs
+++ Assets/Scripts/TileBoard.cs
@@ -85,7 +85,11 @@ public class TileBoard : MonoBehaviour
         {
             if(adjacent.occupied)
             {
-                //TODO : merging
+                if(CanMerge(tile,adjacent.tile))
+                {
+                    Merge(tile, adjacent.tile);
+                    return true;
+                }
                 break;
             }
 
@@ -101,6 +105,35 @@ public class TileBoard : MonoBehaviour
         return false;
     }
 
+
+    private bool CanMerge(Tile a, Tile b)
+    {
+        return a.number == b.number && !b.locked;
+    }
+
+    private void Merge(Tile a , Tile b)
+    {
+        tiles.Remove(a);
+        a.Merge(b.cell);
+
+        int index = Mathf.Clamp(IndexOf(b.state) + 1, 0, tileStates.Length - 1);
+        int number = b.number * 2;
+
+        b.SetState(tileStates[index], number);
+    }
+
+    private int IndexOf(TileState state)
+    {
+        for(int i = 0; i < tileStates.Length; i++ )
+        {
+            if(state == tileStates[i])
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     private IEnumerator WaitForChanges()
     {
         waiting = true;
@@ -109,7 +142,15 @@ public class TileBoard : MonoBehaviour
 
         waiting = false;
 
-        //TODO : create new tile
+        foreach(var tile in tiles)
+        {
+            tile.locked = false; 
+        }
+
+        if(tiles.Count != grid.size)
+        {
+            CreateTile();
+        }
         //TODO : check for ther gameover
     }
 
